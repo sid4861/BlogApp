f831283003d099c596fc41c55984e7d1f5432424
Siddharth Lodha <siddhalo@amdocs.com>
2020-01-06 02:13:55 -0800

    2nd c

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+app = express();
+app.use(express.static("public"));
+
+
+var ejs = require("ejs");
+
+var mongoose = require("mongoose");
+
+// app config
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended : true}));
+mongoose.connect("mongodb://localhost:27017/BlogApp", { useNewUrlParser: true });
+
+//mongoose config
+var blogPostSchema = new mongoose.Schema({
+    title : String,
+    image : String,
+    body : String,
+    createdDate : {type :Date, default : Date.now}
+});
+
+var BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+BlogPost.create({title : "test blog", 
+image : "https://cdn2.howtostartanllc.com/images/business-ideas/business-idea-images/Campground.webp",
+body : "sample post"}, function(err, post){
+
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("created");
+    }
+});
+
+
+//routes
+
+app.listen(3000);
