2eadf14f54a4b4e9be23b641eb869e18c2ceef58
Siddharth Lodha <siddhalo@amdocs.com>
2020-01-09 07:22:49 -0800

    delete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ app = express();
 app.use(express.static("public"));
 
 var methodOverride = require("method-override");
+var experssSanitizer = require("express-sanitizer");
 
 app.use(methodOverride("_method"));
 var ejs = require("ejs");
@@ -12,6 +13,7 @@ var mongoose = require("mongoose");
 // app config
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(experssSanitizer());
 mongoose.connect("mongodb://localhost:27017/BlogApp", { useNewUrlParser: true });
 
 //mongoose config
@@ -65,6 +67,7 @@ app.get("/blogs/new", function(req, res){
 //create route, called within new route
 
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     console.log(req.body.blog);
     BlogPost.create(req.body.blog, function(err, newPost){
         if(err){
@@ -123,7 +126,21 @@ app.put("/blogs/:id", function(req, res){
         }
 
         else{
-              res.redirect("/blogs/")  
+              res.redirect("/blogs/"+req.params.id) ;
+        }
+    });
+});
+
+//delete route
+
+app.delete("/blogs/:id", function(req, res){
+    BlogPost.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/blogs");
+        }
+        else{
+            res.redirect("/blogs");
         }
     });
 });
